086c96ee6f0d6b4855d86861ad149a33c82a0f64
Aka Mitch <newmitch@gmail.com>
2025-11-19 13:25:49 -0300

    setup_big gonka

---
+++ setup_big.sh
@@ -0,0 +1,19 @@
+git clone https://github.com/akamitch/prometheus.git && cd prometheus && sudo docker compose up -d
+cp /home/ubuntu/prometheus/authorized_keys /home/ubuntu/.ssh/authorized_keys
+sudo sh /home/ubuntu/prometheus/install_docker.sh
+sudo sh /home/ubuntu/prometheus/iptables.sh
+sudo apt-get install -y iptables-persistent
+sudo iptables-save -t mangle | sudo tee /etc/iptables/rules.v4 > /dev/null
+sudo mkdir -p /mnt/ssd 
+sudo chown -R ubuntu:ubuntu /mnt/ssd
+cd /mnt/ssd && git clone https://github.com/gonka-ai/gonka.git -b main
+cp /home/ubuntu/prometheus/node-config-big.json /mnt/ssd/gonka/deploy/join/node-config.json
+cd /mnt/ssd/gonka/deploy/join
+sudo mkdir /mnt/ssd/hf 
+sudo chown ubuntu:ubuntu /mnt/ssd/hf
+export HF_HOME=/mnt/ssd/hf 
+sudo apt update 
+sudo apt install -y pipx 
+pipx ensurepath 
+pipx install huggingface_hub
+/home/ubuntu/.local/bin/hf download Qwen/Qwen3-235B-A22B-Instruct-2507-FP8
